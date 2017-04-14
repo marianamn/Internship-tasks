0619ded3a1e511710a28847f1a5073af0c2d6268
Mariana Naidenova <mariananaidenova@mail.bg>
2017-04-14 11:39:50 +0300

    add cloudflare library for IE Promise recognition

--- 02.randomuser-me-task/js/requester.js
+++ 02.randomuser-me-task/js/requester.js
@@ -1,9 +1,9 @@
 var requester = (function() {
     function getJSON(url) {
-        var promise = new Promise((resolve, reject) => {
+        var promise = new Promise(function(resolve, reject) {
 
             $.ajax({
-                url,
+                url: url,
                 method: "GET",
                 contentType: "application/json",
                 success: function(response) {
