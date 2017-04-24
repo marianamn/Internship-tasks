b623a6f0bafe4d8090317da448cab162a9f343d7
marianamn <mariananaidenova@mail.bg>
2017-04-24 19:51:36 +0300

    daily update

--- 04.view-layouts-task/task-1-view-layouts/js/script.js
+++ 04.view-layouts-task/task-1-view-layouts/js/script.js
@@ -1,21 +1,45 @@
-var views = document.querySelectorAll("#container .view");
+var views = document.querySelectorAll("#container .view"),
+    nav = document.getElementById("navigation");
 
-var currentView = views[0];
-currentView.classList.add("current");
+nav.addEventListener("click", changeView);
 
-function clickedNavigationLink(clickedLink, activeViewNumber) {
-    clickedLink.addEventListener("click", function() {
-        currentView.classList.remove("current");
+function changeView(e) {
+    var currentIndex,
+        nextIndex,
+        listItems,
+        currentView,
+        nextView;
 
-        currentView = views[activeViewNumber];
-        currentView.classList.add("current");
-    });
+    e.preventDefault();
+
+    if (e.target.nodeName.toLowerCase() === 'a') {
+        listItems = nav.querySelectorAll('li');
+        currentIndex = findActiveItemIndex(listItems);
+
+        // remove active class from current slide
+        nav.querySelector('.js-active').classList.remove('js-active');
+
+        // mark which is the next slide
+        e.target.classList.add('js-active');
+        nextIndex = findActiveItemIndex(listItems);
+
+        currentView = views[currentIndex];
+        nextView = views[nextIndex];
+
+        if (currentIndex > nextIndex) {
+            currentView.className = 'view center-to-right';
+            nextView.className = 'view left-to-right js-active';
+        } else if (currentIndex < nextIndex) {
+            currentView.className = 'view center-to-left';
+            nextView.className = 'view right-to-left js-active';
+        }
+    }
 }
 
-var firstLink = document.getElementsByClassName("first-link")[0];
-var secondLink = document.getElementsByClassName("second-link")[0];
-var thirdLink = document.getElementsByClassName("third-link")[0];
-
-clickedNavigationLink(firstLink, 0);
-clickedNavigationLink(secondLink, 1);
-clickedNavigationLink(thirdLink, 2);
+function findActiveItemIndex(elems) {
+    for (var i = 0; i < elems.length; i++) {
+        if (elems[i].querySelector('.js-active')) {
+            return i;
+        }
+    }
+}
